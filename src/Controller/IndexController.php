9c3e99e27dc974a9553b61f37a2807b6e022df2d
Oceane jean-François <oceanejf974@gmail.com>
2024-12-06 06:32:40 +0100

    projet finis

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
+
+          $articles = $articleRepository->findBy([], ['dateCreation' => 'DESC'], 3);
         return $this->render('index/index.html.twig', [
+            'articles' => $articles,
             'controller_name' => 'IndexController',
         ]);
     }
